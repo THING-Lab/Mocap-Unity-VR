73dced24a9f46204bb5dc8cae9af4dfacb7b108b
Peter Gyory <ptgyory@gmail.com>
2019-05-22 10:48:29 -0600

    Add basic sensor fusion

---
+++ QTM-Unity-VR/Assets/Scripts/QTMObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QTMRealTimeSDK;
+using QualisysRealTime.Unity;
+
+// This class is very similar to QTM's RT Object,
+// but I might need to make changes so I'm keeping it for now
+public class QTMObject : MonoBehaviour
+{
+    public string objectName;
+    public Vector3 rotationOffset;
+    public float updateWindow = 0f;
+    private float updateTimer = 0f;
+
+    private bool ShouldOffsetRotation {
+        get { return !(rotationOffset.x == 0 && rotationOffset.y == 0 && rotationOffset.z == 0); }
+    }
+
+    private RTClient rtClient;
+    // Start is called before the first frame update
+    void Start() {
+        rtClient = RTClient.GetInstance();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (updateTimer > 0) updateTimer -= Time.deltaTime;
+
+        if (updateTimer <= 0 && rtClient.GetStreamingStatus()) {
+            SixDOFBody trackedObj = rtClient.GetBody(objectName);
+            if (!float.IsNaN(trackedObj.Position.sqrMagnitude)) {
+                transform.position = trackedObj.Position * 3;
+
+                if (ShouldOffsetRotation) {
+                    transform.rotation = trackedObj.Rotation * Quaternion.Euler(rotationOffset);
+                } else {
+                    transform.rotation = trackedObj.Rotation;
+                }
+            }
+
+            updateTimer = updateWindow;
+        }
+    }
+}
---
+++ QTM-Unity-VR/Assets/Scripts/RotationCorrection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationCorrection : MonoBehaviour
+{
+    public Transform rotationSource;
+    public QTMObject outputTarget;
+
+    // Update is called once per frame
+    void Update() {
+        outputTarget.rotationOffset = Quaternion.Inverse(rotationSource.localRotation).eulerAngles;
+    }
+}
